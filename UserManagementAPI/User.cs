b7b9efa02c87d6e37248f42c7226227aafa72434
Oscar Daniel Otero <ingoscardan@gmail.com>
2025-01-09 11:04:58 -0600

    Add annotations to user model for validations

--- UserManagementAPI/User.cs
+++ UserManagementAPI/User.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UserManagementAPI;
 
 public class User
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
+    public string LastName { get; set; } = null!;
 }
